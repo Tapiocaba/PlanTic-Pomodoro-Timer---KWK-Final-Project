d7bb38737855d12f4efe14be22c1c298204081dc
Tapiocaba <66091764+Tapiocaba@users.noreply.github.com>
2021-07-06 12:56:40 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,165 @@
+var sessionSeconds = "00";
+var sessionMinutes = 25;
+
+//audio
+var alarm = new Audio("alarm.mp3");
+
+function studyTimer() {
+  // Change the minutes and seconds to starting time
+  session_minutes = 24;
+  session_seconds = 59;
+
+  // Add the seconds and minutes to the page
+  document.getElementById("minutes").innerHTML = session_minutes;
+  document.getElementById("seconds").innerHTML = session_seconds;
+
+  // Start the countdown
+  var minutes_interval = setInterval(minutesTimer, 60000);
+  var seconds_interval = setInterval(secondsTimer, 1000);
+
+  // Functions
+  // Function for minute counter
+  function minutesTimer() {
+    session_minutes = session_minutes - 1;
+    document.getElementById("minutes").innerHTML = session_minutes;
+  }
+
+  // Function for second counter
+  function secondsTimer() {
+    session_seconds = session_seconds - 1;
+    document.getElementById("seconds").innerHTML = session_seconds;
+
+    // Check if the seconds and minutes counter has reached 0
+    // If reached 0 then end the session
+    if (session_seconds <= 0) {
+      if (session_minutes <= 0) {
+        // Clears the interval i.e. stops the counter
+        clearInterval(minutes_interval);
+        clearInterval(seconds_interval);
+
+        // Add the message to the html
+        document.getElementById("done").innerHTML =
+          "Session Completed!! Take a Break";
+
+        // Make the html message div visible
+        document.getElementById("done").classList.add("show_message");
+
+        // PLay the bell sound to tell the end of session
+        bell.play();
+      }
+
+      // Reset the session seconds to 60
+      session_seconds = 60;
+    }
+  }
+}
+
+function longBreak() {
+  // Change the minutes and seconds to starting time
+  session_minutes = 14;
+  session_seconds = 59;
+
+  // Add the seconds and minutes to the page
+  document.getElementById("minutes").innerHTML = session_minutes;
+  document.getElementById("seconds").innerHTML = session_seconds;
+  
+  // Start the countdown
+  var minutes_interval = setInterval(minutesTimer, 60000);
+  var seconds_interval = setInterval(secondsTimer, 1000);
+
+  // Functions
+  // Function for minute counter
+  function minutesTimer() {
+    session_minutes = session_minutes - 1;
+    document.getElementById("minutes").innerHTML = session_minutes;
+  }
+
+  // Function for second counter
+  function secondsTimer() {
+    session_seconds = session_seconds - 1;
+    document.getElementById("seconds").innerHTML = session_seconds;
+
+    // Check if the seconds and minutes counter has reached 0
+    // If reached 0 then end the session
+    if (session_seconds <= 0) {
+      if (session_minutes <= 0) {
+        // Clears the interval i.e. stops the counter
+        clearInterval(minutes_interval);
+        clearInterval(seconds_interval);
+
+        // Add the message to the html
+        document.getElementById("done").innerHTML =
+          "Break completed! Have fun studying!";
+
+        // Make the html message div visible
+        document.getElementById("done").classList.add("show_message");
+
+        // PLay the bell sound to tell the end of session
+        bell.play();
+      }
+
+      // Reset the session seconds to 60
+      session_seconds = 60;
+    }
+  }
+}
+
+function shortBreak() {
+  // Change the minutes and seconds to starting time
+  session_minutes = 4;
+  session_seconds = 59;
+
+  // Add the seconds and minutes to the page
+  document.getElementById("minutes").innerHTML = session_minutes;
+  document.getElementById("seconds").innerHTML = session_seconds;
+  
+  // Start the countdown
+  var minutes_interval = setInterval(minutesTimer, 60000);
+  var seconds_interval = setInterval(secondsTimer, 1000);
+
+  // Functions
+  // Function for minute counter
+  function minutesTimer() {
+    session_minutes = session_minutes - 1;
+    document.getElementById("minutes").innerHTML = session_minutes;
+  }
+
+  // Function for second counter
+  function secondsTimer() {
+    session_seconds = session_seconds - 1;
+    document.getElementById("seconds").innerHTML = session_seconds;
+
+    // Check if the seconds and minutes counter has reached 0
+    // If reached 0 then end the session
+    if (session_seconds <= 0) {
+      if (session_minutes <= 0) {
+        // Clears the interval i.e. stops the counter
+        clearInterval(minutes_interval);
+        clearInterval(seconds_interval);
+
+        // Add the message to the html
+        document.getElementById("done").innerHTML =
+          "Break completed! Have fun studying!";
+
+        // Make the html message div visible
+        document.getElementById("done").classList.add("show_message");
+
+        // PLay the bell sound to tell the end of session
+        bell.play();
+      }
+
+      // Reset the session seconds to 60
+      session_seconds = 60;
+    }
+  }
+}
+
+//plant button PLZ WORK 
+$(".button").click(function() {
+	$(".stem").addClass("rain");
+	
+	setTimeout(function () { 
+		$('.stem').removeClass('rain');
+	}, 1500000);  
+});
+
